266cea502436b7b89e5a1df0c47087664f88dae3
Mario Moraes <moraes_mario@hotmail.com>
2022-10-30 11:52:23 -0300

    File

--- lib/app/repositories/user/user_repository_impl.dart
+++
@@ -1,8 +0,0 @@
-import 'package:app_mcip/app/models/user_model.dart';
-
-import './user_repository.dart';
-
-class UserRepositoryImp implements UserRepository {
-  @override
-  Future<UserModel> getUser(String empresaId, String login) async {}
-}
